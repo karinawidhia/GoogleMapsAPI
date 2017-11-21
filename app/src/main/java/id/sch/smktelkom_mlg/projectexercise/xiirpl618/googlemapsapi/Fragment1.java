f40fa0202d982c1bf6f3e7b429dad5c71f66ae4e
Karina Widhia Nugrahini <karinwidhia@gmail.com>
2017-11-21 07:15:14 +0700

    Commit Finish

--- app/src/main/java/id/sch/smktelkom_mlg/projectexercise/xiirpl618/googlemapsapi/Fragment1.java
+++ app/src/main/java/id/sch/smktelkom_mlg/projectexercise/xiirpl618/googlemapsapi/Fragment1.java
@@ -1,17 +1,37 @@
 package id.sch.smktelkom_mlg.projectexercise.xiirpl618.googlemapsapi;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Fragment1 extends Fragment {
+public class Fragment1 extends Fragment implements OnMapReadyCallback {
+
+    static final CameraPosition SGH = CameraPosition.builder()
+            .target(new LatLng(-6.887088, 111.670994))
+            .zoom(10)
+            .bearing(0)
+            .tilt(45)
+            .build();
+    GoogleMap m_map;
+    boolean mapReady = false;
+    LatLng swjjr = new LatLng(-7.972649, 112.658601);
 
 
     public Fragment1() {
@@ -26,4 +46,21 @@ public class Fragment1 extends Fragment {
         return inflater.inflate(R.layout.fragment_fragment1, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(SGH));
+        map.addCircle(new CircleOptions()
+                .center(swjjr)
+                .radius(1000)
+                .strokeColor(Color.GREEN)
+                .fillColor(Color.argb(64, 0, 255, 0)));
+    }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/projectexercise/xiirpl618/googlemapsapi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/projectexercise/xiirpl618/googlemapsapi/MainActivity.java
@@ -1,9 +1,8 @@
 package id.sch.smktelkom_mlg.projectexercise.xiirpl618.googlemapsapi;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,14 +21,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -79,23 +77,37 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        changePage(id);
 
-        if (id == R.id.nav_camera) {
+        return true;
+    }
+
+    private void changePage(int id) {
+        Fragment fragment = null;
+
+        if (id == R.id.nav_map_mpde) {
+            fragment = new FragmentMap();
+            setTitle("Map Mode");
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_moving_map) {
+            fragment = new FragmentMove();
+            setTitle("Moving Map");
+        } else if (id == R.id.nav_place_marker) {
+            fragment = new FragmentPlace();
+            setTitle("Place Marker");
+        } else if (id == R.id.nav_draw_circle) {
+            fragment = new Fragment1();
+            setTitle("Draw Circle");
+        } else if (id == R.id.nav_street_view) {
+            fragment = new FragmentStreet();
+            setTitle("Street View");
         }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commitNow();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+
+
     }
 }
